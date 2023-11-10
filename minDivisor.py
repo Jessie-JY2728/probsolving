b594e1e998b5be9e2e3c79b52b9487bfc99379bd
Jessie-JY2728 <140844394+Jessie-JY2728@users.noreply.github.com>
2023-11-10 13:05:43 -0500

    Update minDivisor.py

--- minDivisor.py
+++ minDivisor.py
@@ -1,1 +1,16 @@
+# Question:
+# given int[] arr, int thres, find an int x,
+# such that when each elem of arr takes ceiling division with x
+# the resulting array sums up to less than or equal to thres
+# if impossible, return -1
+import math
 
+def minDivisor(arr, thres):
+  ans = floor(sum(arr) / thres)
+  n = len(arr)
+  for x in range(ans, ans + n):
+    newsum = sum([ceil(i / x) for i in arr])
+    if newsum <= thres:
+      return newsum
+  return -1
+  
